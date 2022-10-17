5201ae40ab68e39291e5523ec62f7cd940d90977
FomenkoVD <fom.stud@yandex.ru>
2022-10-17 14:40:59 +0500

    new folder

---
+++ 2022.10.17 - Первая Лаба/LeLaboratoriqueWorkel-01.c
@@ -0,0 +1,13 @@
+#include <math.h>
+
+int main() {
+    float x, y, z;
+    printf("Enter x -> ");
+    scanf("%f", &x);
+    printf("Enter y -> ");
+    scanf("%f", &y);
+    printf("Enter z -> ");
+    scanf("%f", &z);
+
+    return 0;
+}
