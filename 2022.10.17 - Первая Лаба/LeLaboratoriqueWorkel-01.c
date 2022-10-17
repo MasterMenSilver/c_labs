9fce2d8aa3eb4ae1c404cb04dc34b442f3ed889c
FomenkoVD <fom.stud@yandex.ru>
2022-10-17 14:54:49 +0500

    Lab1 Loaded

--- 2022.10.17 - Первая Лаба/LeLaboratoriqueWorkel-01.c
+++ 2022.10.17 - Первая Лаба/LeLaboratoriqueWorkel-01.c
@@ -1,13 +1,44 @@
+#include <stdio.h>
 #include <math.h>
+
+// Subject: Lab-01_Task-02;
+// Last: 2022.10.17 14:50
 
 int main() {
     float x, y, z;
-    printf("Enter x -> ");
-    scanf("%f", &x);
-    printf("Enter y -> ");
-    scanf("%f", &y);
-    printf("Enter z -> ");
-    scanf("%f", &z);
+    int PreEnter = 0;
+
+    if (PreEnter == 1) {
+        printf("Enter x: ");
+        scanf("%f", &x);
+        printf("Enter y: ");
+        scanf("%f", &y);
+        printf("Enter z: ");
+        scanf("%f", &z);
+    } else {
+        x = 25.0f;
+        y = 16.0f;
+        z = 9.0f;
+    }
+
+    // Plus
+    float xyp = x + y;
+    float yzp = y + z;
+    float zxp = z + x;
+
+    // Minus
+    float xym = x - y;
+    float yzm = y - z;
+    float zxm = z - x;
+
+    if (x > yzp & x > yzm)
+        printf("%f", x);
+    else if (y > zxp & x > zxm)
+        printf("%f", y);
+    else if (z > xyp & x > xym)
+        printf("%f", z);
+    else
+        printf("wtf dude?");
 
     return 0;
 }
